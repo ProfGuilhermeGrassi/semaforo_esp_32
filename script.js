b67a06d1130e923718cf9d5c3dc1f83554f32f18
ProfGuilhermeGrassi <guilherme.g@docente.senai.br>
2025-07-23 10:45:30 -0300

    conectando

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let clientWeb = null
+
+const clientId = "Esp32" + Math.floor(Math.random() * 900) + 100
+clientWeb = new Paho.MQTT.Client("broker.hivemq.com", 8884, clientId)
+
+clientWeb.connect({
+    useSSL: true, 
+    onSuccess:function(){
+        alert("Conectado com sucesso")
+    },
+    onFailure: function() {
+        alert ("A conexão falhou")
+    }
+})
+
 
 
 function ligarVermelho(){
